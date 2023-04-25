8599eccc72b139c58c936200a2c6b8a6c43aa4e2
Alejandro Pedraza <alejandro@buoyant.io>
2023-04-25 11:44:51 -0500

    Test fixups
    Signed-off-by: Alejandro Pedraza <alejandro@buoyant.io>

--- test/linkerd/install.sh
+++ test/linkerd/install.sh
@@ -2,24 +2,33 @@
 
 set -o errexit
 
-LINKERD_VER="stable-2.11.2"
+LINKERD_VER="stable-2.13.2"
+LINKERD_SMI_VER="0.2.0"
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 mkdir -p ${REPO_ROOT}/bin
 
 curl -SsL https://github.com/linkerd/linkerd2/releases/download/${LINKERD_VER}/linkerd2-cli-${LINKERD_VER}-linux-amd64 > ${REPO_ROOT}/bin/linkerd
 chmod +x ${REPO_ROOT}/bin/linkerd
+curl -SsL https://github.com/linkerd/linkerd-smi/releases/download/v${LINKERD_SMI_VER}/linkerd-smi-${LINKERD_SMI_VER}-linux-amd64 > ${REPO_ROOT}/bin/linkerd-smi
+chmod +x ${REPO_ROOT}/bin/linkerd-smi
 
 echo ">>> Installing Linkerd ${LINKERD_VER}"
+${REPO_ROOT}/bin/linkerd install --crds | kubectl apply -f -
 ${REPO_ROOT}/bin/linkerd install | kubectl apply -f -
 ${REPO_ROOT}/bin/linkerd check
 
+echo ">>> Installing Linkerd SMI"
+${REPO_ROOT}/bin/linkerd-smi install | kubectl apply -f -
+${REPO_ROOT}/bin/linkerd-smi check
+
 echo ">>> Installing Linkerd Viz"
 ${REPO_ROOT}/bin/linkerd viz install | kubectl apply -f -
+kubectl -n linkerd-viz rollout status deploy
 ${REPO_ROOT}/bin/linkerd viz check
 
 echo '>>> Installing Flagger'
 kubectl apply -k ${REPO_ROOT}/kustomize/linkerd
 
-kubectl -n linkerd set image deployment/flagger flagger=test/flagger:latest
-kubectl -n linkerd rollout status deployment/flagger
+kubectl -n flagger-system set image deployment/flagger flagger=test/flagger:latest
+kubectl -n flagger-system rollout status deployment/flagger
